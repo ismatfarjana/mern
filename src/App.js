19c3d72e67195aad79fc190e83caa8122fa70cc8
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-11 10:14:39 +1100

    Relocate components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
 
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
 function App() {
   return (
     <div className="App">
