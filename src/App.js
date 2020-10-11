dcab4236ac1ebcc9833b7a297cb1ff1411e9542b
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-12 10:46:22 +1100

    Create authReducer.js file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from "react";
-// import logo from './logo.svg';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+
+//for redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1>Hello</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <h1>Hello</h1>
+            </header>
+            <body>
+              <Navbar />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </body>
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
