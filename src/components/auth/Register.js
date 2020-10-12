281ae7b5e94480b872757eeabb165331bc0480a8
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-13 05:40:13 +1100

    Link redux to login and register component

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+import classnames from "classnames";
+
 class Register extends Component {
   constructor() {
     super();
@@ -11,6 +16,16 @@ class Register extends Component {
       errors: {}
     };
   }
+
+  //------//
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.state({
+        errors: nextProps.errors
+      });
+    }
+  }
+
   onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
@@ -22,7 +37,7 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+    this.props.registerUser(newUser, this.props.history);
   };
   render() {
     const { errors } = this.state;
@@ -50,8 +65,13 @@ class Register extends Component {
                   error={errors.name}
                   id="name"
                   type="text"
+                  className={classnames("", {
+                    invalid: errors.name
+                  })}
                 />
+
                 <label htmlFor="name">Name</label>
+                <span className="red-text">{errors.name}</span>
               </div>
               <div className="input-field col s12">
                 <input
@@ -60,8 +80,13 @@ class Register extends Component {
                   error={errors.email}
                   id="email"
                   type="email"
+                  className={classnames("", {
+                    invalid: errors.email
+                  })}
                 />
+
                 <label htmlFor="email">Email</label>
+                <span className="red-text">{errors.email}</span>
               </div>
               <div className="input-field col s12">
                 <input
@@ -70,8 +95,12 @@ class Register extends Component {
                   error={errors.password}
                   id="password"
                   type="password"
+                  className={classnames("", {
+                    invalid: errors.password
+                  })}
                 />
                 <label htmlFor="password">Password</label>
+                <span className="red-text">{errors.password2}</span>
               </div>
               <div className="input-field col s12">
                 <input
@@ -104,4 +133,19 @@ class Register extends Component {
     );
   }
 }
-export default Register;
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+//mapStateToProps allows us to get our state from Redux and map it to props which we can use inside components. This allows us to call this.props.auth or this.props.errors within our Register component.
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+// connect connects our React components to our Redux store provided by the Provider component
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
